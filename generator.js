6cee41c7f2c58d97d5a3a756dc179871aa7c1bda
MattHelton <mjoelhelton@me.com>
2019-01-02 22:41:08 -0500

    Adds index.html with a Generate Password Button. On click, a 16 character password made up of letters and numbers is generated and logged to the console. First character is always a letter.

--- generator.js
+++ generator.js
@@ -1,20 +1,35 @@
+let generator = document.getElementById('generate')
+
+
+
 const digitArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 let pwArray = []
-let random = (max) => {
+
+function randomCharacter (max) {
 return Math.floor(Math.random() * Math.floor(max))
 }
 
-let generateDigit = () => {
-    let digit = digitArray[random(36)]
-    return digit
+function generate (num) {
+    return digitArray[randomCharacter(num)]
 }
 
-let passwordGen = () => {
-    let i
-    for( i = 0; i < 16; i++)
-    pwArray.push(generateDigit())
-
+function assignFirstDigit () {
+    let first = digitArray[generate(25)]
+    pwArray.push(first)
 }
 
-passwordGen()
-console.log(pwArray.join(''))
+function buildPasswordArray () {
+    for (let i = 0; i < 15; i++) {
+        pwArray.push(generate(36))
+    }
+}
+
+function printPassword (array) {
+    console.log(array.join(''))
+}
+
+generator.addEventListener('click', function () {
+    assignFirstDigit()
+    buildPasswordArray()
+    printPassword(pwArray)
+})
